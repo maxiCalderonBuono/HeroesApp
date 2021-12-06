e90d1206b03e2038a0333f23476ed702b1c4f63c
Maximiliano <calderonmaxi@outlook.com>
2021-12-06 11:28:12 -0300

    redirects for netlify added

--- src/components/heroes/HeroesList.js
+++ src/components/heroes/HeroesList.js
@@ -9,7 +9,7 @@ export const HeroesList = ({ publisher }) => {
   return (
     <div className= "row row-cols-1 row-cols-md-3 g-4 animate__animated animate__fadeIn">
       {heroes.map((heroe) => (
-        <HeroeCard key={heroe.id} {...heroe} />
+         <HeroeCard key={heroe.id} {...heroe} />
       ))}
     </div>
   );
