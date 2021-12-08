405f3a73effc5255faedf87a8abd2f7fbfb0e8fe
Maximiliano <calderonmaxi@outlook.com>
2021-12-08 02:00:36 -0300

    reveal animation added

--- src/components/heroes/HeroesList.js
+++ src/components/heroes/HeroesList.js
@@ -1,16 +1,19 @@
 import React, { useMemo } from "react";
-import 'animate.css'
+import "animate.css";
 import { getHeroeByPublisher } from "../../selectors/getHeroeByPublisher";
 import { HeroeCard } from "./HeroeCard";
+import Fade from "react-reveal/Fade";
 
 export const HeroesList = ({ publisher }) => {
-  const heroes = useMemo(()=> getHeroeByPublisher(publisher), [publisher]);
+  const heroes = useMemo(() => getHeroeByPublisher(publisher), [publisher]);
 
   return (
-    <div className= "row row-cols-1 row-cols-md-3 g-4 animate__animated animate__fadeIn">
-      {heroes.map((heroe) => (
-         <HeroeCard key={heroe.id} {...heroe} />
-      ))}
+    <div className="row row-cols-1 row-cols-md-3 g-4 animate__animated animate__fadeIn">
+      <Fade bottom>
+        {heroes.map((heroe) => (
+          <HeroeCard key={heroe.id} {...heroe} />
+        ))}
+      </Fade>
     </div>
   );
 };
